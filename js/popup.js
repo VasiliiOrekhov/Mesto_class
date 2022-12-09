d3874b5cbd9db509bdefa5d71d91e3d37bed538c
Your Name <vasilii.orekhov95@gmail.com>
2022-12-09 21:07:55 +0300

    sprint7_5of6

--- js/popup.js
+++ js/popup.js
@@ -9,6 +9,7 @@ import {
   author,
   newCardHeandler,
   editProfileHeandler,
+  validError,
 } from './constants';
 //форма создания новой карточки
 export function openPlaceForm(event) {
@@ -19,11 +20,11 @@ export function openPlaceForm(event) {
 export function closePlaceForm(event) {
   popupPlace.classList.remove('popup-opened');
   popupBackground.classList.remove('popup-background-opened');
+  validError.textContent = '';
 }
 //валидация формы новой карточки
 export function newCardVallid(event) {
-  if (name.value.length === 0 || link.value.length === 0) {
-    console.log('ddddd');
+  if (name.value.length < 2 || link.value.length === 0) {
     newCardHeandler.setAttribute('disabled', true);
     newCardHeandler.classList.remove('popup__button_no');
   } else {
@@ -31,6 +32,24 @@ export function newCardVallid(event) {
     newCardHeandler.classList.add('popup__button_no');
   }
 }
+export function formValid(event) {
+  if (name.validity.valid) {
+    validError.textContent = '';
+    validError.className = 'popup__error';
+  } else {
+    showError();
+  }
+}
+function showError() {
+  if (name.validity.valueMissing) {
+    // Если поле пустое
+    validError.textContent = 'Это обязательное поле';
+  } else if (name.validity.tooShort) {
+    // Если содержимое короткое
+    validError.textContent = 'Должно быть от 2 до 30 символов';
+  }
+}
+
 //форма изменения профиля
 export function openProfileForm(event) {
   popupProfile.classList.add('popup-opened');
@@ -54,8 +73,9 @@ export function editProfileVallid(event) {
 //форма открывания карточки
 export function openCardForm(event) {
   if (event.target.classList.contains('place-card__image')) {
-    console.log(event.target);
-    console.log(event.target.getAttribute('style'));
+    //поменять с bgimg на img
+    // console.log(event.target.style.backgroundImage);
+    // console.log(event.target.getAttribute('style'));
     const backgroundImg = event.target.getAttribute('style');
     popupOpenImg.setAttribute('style', backgroundImg);
     popupOpenImg.classList.add('popup-opened-img');
